46ca7ff236901f337a187919f018ca424934ff70
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-02-04 15:40:01 +0000

    added nodemon

--- notes.md
+++ notes.md
@@ -1,4 +1,19 @@
 # set up git 
 
-npm install --save
+```npm install --save```
+
+## to start application
+
+``` node index.js ```
+
+## nodemon
+
+``` npm install -g nodemon```
+``` nodemon ```
+
+## curl, a command line tool 
+
+that allows me to make requests
+
+``` curl localhost:8081```
 
--- warbler-server/index.js
+++ warbler-server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const PORT = 8081;
+// set up some modules, set the PORT number and created a very simple error handler
 
 app.use(cors())
 app.use(bodyParser.json()) 
@@ -12,5 +14,12 @@ app.use(bodyParser.json())
 // then run this function
 
 app.use(function(req, res, next) {
-    
+    let err = new Error("Not Found ")
+    err.status = 404;
+    next(err);
 });
+
+app.listen(PORT, function() {
+    // using a lot of ES2015 in this application
+    console.log(`Server is starting on port ${PORT}`);
+} );
